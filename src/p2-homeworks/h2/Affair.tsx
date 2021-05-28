1431613d6b5ff042e6e4b0437348d88b1ad50249
TatsianaSamtsova <tanyaminsk92@gmail.com>
2021-05-28 12:49:18 +0300

    Initial commit

---
+++ src/p2-homeworks/h2/Affair.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {AffairType} from "./HW2";
+import s from './Affairs.module.css'
+
+type AffairPropsType = {
+    // key не нужно типизировать
+    affair: AffairType
+    deleteAffairCallback: (_id: number)=> void
+}
+
+function Affair(props: AffairPropsType) {
+    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}// need to fix
+    const deleteButtonClass = `${s.button} ${s.del}`
+    return (
+        <div className={s.affair}>
+           <div>name: {props.affair.name}</div>
+           <div>priority: {props.affair.priority}</div>
+
+            <button className={deleteButtonClass} onClick={deleteCallback}>X</button>
+        </div>
+    )
+}
+
+export default Affair
---
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -0,0 +1,46 @@
+import React, {ChangeEvent, useState} from 'react'
+import Greeting from './Greeting'
+import {UserType} from "./HW3";
+
+type GreetingContainerPropsType = {
+    users: Array<UserType>
+    addUserCallback: (name: string)=> void
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+// уровень локальной логики
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
+    const [name, setName] = useState("") // need to fix any
+    const [error, setError] = useState<string>('') // need to fix any
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+        setError("")// need to fix
+    }
+    const addUser = () => {
+        if (name.trim() !== "") {
+            addUserCallback(name)
+            setName("")
+        }
+        else {
+            setError("Error")
+        }
+    }
+
+    const totalUsers = users.length // need to fix
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            error={error}
+            totalUsers={totalUsers}
+        />
+    )
+}
+
+export default GreetingContainer
