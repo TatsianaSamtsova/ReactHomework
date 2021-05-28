142c0a258845ef0456d66ce2d6e058d8fdf83042
TatsianaSamtsova <tanyaminsk92@gmail.com>
2021-05-28 14:59:20 +0300

    Updates

---
+++ precache-manifest.928bc12673e1bd440192f9a7b1f25ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659dfc8368402862d2c57a9eb5b2741f",
+    "url": "/ReactHomework/index.html"
+  },
+  {
+    "revision": "850c531463cd11865738",
+    "url": "/ReactHomework/static/css/main.028eb42a.chunk.css"
+  },
+  {
+    "revision": "4fa44a6a1782edcc2395",
+    "url": "/ReactHomework/static/js/2.3cb3f5c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactHomework/static/js/2.3cb3f5c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "850c531463cd11865738",
+    "url": "/ReactHomework/static/js/main.7f08c1fb.chunk.js"
+  },
+  {
+    "revision": "05e2de67659115630dfa",
+    "url": "/ReactHomework/static/js/runtime-main.675996c5.js"
+  }
+]);
